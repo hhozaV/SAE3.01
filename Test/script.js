4858d0c95012d52d0398a840925b122a30cbf882
cp069212 <Clement_Papillon@etu.u-bourgogne.fr>
2023-11-28 16:20:00 +0100

    Check les reponses

--- Test/script.js
+++
@@ -1,6 +0,0 @@
-const scrollButton = document.querySelector(".scroll-button");
-const navbar = document.querySelector(".navbar");
-
-scrollButton.addEventListener("click", () => {
-    navbar.classList.toggle("active");
-});
