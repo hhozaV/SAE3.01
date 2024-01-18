7f19c9a0e1ce0df3bd75298bbac4b01c09d5c2f1
☎️ <113849533+hhozaV@users.noreply.github.com>
2024-01-18 10:53:36 +0100

    Add about.php + add responsive on survie & about

---
+++ View/about.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Eduquiz</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Montserrat:wght@700;800;900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="../img/cerveau.png" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <?php include "header.php"; ?>
+    </header>
+
+    <div class="about-section">
+        <h1>À propos de Eduquiz</h1>
+        <div class="about-container">
+            <div class="about-items">
+                <h3>Qu'est ce que Eduquiz ?</h3>
+                <p>Eduquiz est un site web qui a pour but d'aider les étudiants à trouver des ressources pour leurs études. Il est possible de trouver des quizz et des leçons sur de nombreux thèmes. Notre mission est de rendre l'apprentissage de l'informatique accessible, engageant et efficace pour tous que vous soyez étudiant, professionnel ou simplement passionné par l'informatique.</p>
+            </div>
+            <div class="about-items">
+                <h3>Qui sommes-nous ?</h3>
+                <p>Nous sommes une équipe de 3 étudiants en 2eme année de BUT Informatique à Nevers. Nous avons créé ce site web dans le cadre d'un projet de fin de semestre.</p>
+            </div>
+            <div class="about-items">
+                <h3>Comment nous contacter ?</h3>
+                <p>Vous pouvez nous contacter par mail à l'adresse suivante : <a href="mailto:">eduquiz@gmail.com</a></p>
+    </div>
