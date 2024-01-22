2be467e3586bc0173e005af31049f7bffbd64c50
Vazohh <59361135+Vazohh@users.noreply.github.com>
2024-01-22 18:23:51 +0100

    Modified last things in CSS

--- View/profil.php
+++ View/profil.php
@@ -30,98 +30,69 @@ include "../Model/requests.php";
     <link rel="icon" href="../img/cerveau.png" type="image/x-icon">
 </head>
 <body>
-    <header>
-        <div class="logo">Eduquiz</div>
-        <div class="hamburger">
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
-        </div>
-        <nav class="nav-bar">
-            <ul>
-                <li><a href="../View/index.php" class="nav-link">Accueil</a></li>
-                <li><a href="../View/themes.php" class="nav-link">Thèmes</a></li>
-                <li><a href="../View/lessons.php" class="nav-link">Leçons</a></li>
-                <li><a href="../View/survie.php" class="nav-link">Mode Survie</a></li>
-
-                <?php
-
-                // Vérifier si l'utilisateur est connecté
-                if (isset($_SESSION["utilisateur_username"])) {
-                    // L'utilisateur est connecté
-                    if (isset($_SESSION["utilisateur_role"]) && $_SESSION["utilisateur_role"] === 'admin') {
-                        // L'utilisateur est un admin
-                        echo '<li><a href="admin_panel.php" class="nav-link connect">Panel Admin</a></li>';
-                    } else {
-                        // L'utilisateur est connecté mais n'est pas admin
-                        echo '<li><a href="profil.php" class="nav-link connect">Profil</a></li>';
+    <div class="profil">
+        <header>
+            <?php include "header.php"; ?>
+        </header>
+        <div class="mainProfil-container">
+            <div class="profil-container">
+                <div class="profile-card">
+                    <img src="../img/profil.svg" alt="Profil" class="profile-image">
+                    <h1>Profil de l'utilisateur</h1>
+                    <p>Nom d'utilisateur : <?php echo htmlspecialchars($user['username']); ?></p>
+                    <p>Email : <?php echo htmlspecialchars($user['email']); ?></p>
+                    <p>Mot de passe : ********</p>
+                    <p>Date d'inscription : <?php echo htmlspecialchars($user['date_inscription']); ?></p>
+                    <a href="../Model/passwordedit.php" class="change-password-button">Changer de MDP</a>
+                    <a href="index.php"">Retourner à l'accueil</a>
+                    <a href="../Model/logout.php">Se déconnecter</a>
+                    <a id="deleteAccountBtn">Supprimer mon compte</a>
+                </div>
+            </div>
+            <div class="scores-container">
+                <h2>Scores</h2>
+                <?php 
+                $current_theme = null;
+                while ($score = $resultat_scores->fetch_assoc()):
+                    if ($score['theme_name'] == 'Survie') {
+                        continue;
                     }
-                } else {
-                    // L'utilisateur n'est pas connecté
-                    echo '<li><a href="../Model/login.php" class="nav-link connect">Se connecter</a></li>';
-                }
+                    if ($score['theme_name'] != $current_theme) {
+                        if ($current_theme !== null) {
+                            echo '</tbody></table>';
+                        }
+                        echo '<h3>' . htmlspecialchars($score['theme_name']) . '</h3>';
+                        echo '<table>
+                                <thead>
+                                    <tr>
+                                        <th>Difficulté</th>
+                                        <th>Score</th>
+                                    </tr>
+                                </thead>
+                                <tbody>';
+                        $current_theme = $score['theme_name'];
+                    }
                 ?>
-            </ul>
-        </nav>
-    </header>
-    <div class="mainProfil-container">
-        <div class="profil-container">
-            <div class="profile-card">
-                <img src="../img/profil.svg" alt="Profil" class="profile-image">
-                <h1>Profil de l'utilisateur</h1>
-                <p>Nom d'utilisateur : <?php echo htmlspecialchars($user['username']); ?></p>
-                <p>Email : <?php echo htmlspecialchars($user['email']); ?></p>
-                <p>Mot de passe : ********</p>
-                <p>Date d'inscription : <?php echo htmlspecialchars($user['date_inscription']); ?></p>
-                <a href="../Model/passwordedit.php" class="change-password-button">Changer de MDP</a>
-                <a href="index.php"">Retourner à l'accueil</a>
-                <a href="../Model/logout.php">Se déconnecter</a>
-                <a id="deleteAccountBtn">Supprimer mon compte</a>
+                    <tr>
+                        <td><?php echo htmlspecialchars($score['difficulty']); ?></td>
+                        <td><?php echo htmlspecialchars($score['score']); ?></td>
+                    </tr>
+                <?php endwhile; ?>
+                    </tbody>
+                </table>
+                <h2>Meilleur score en mode Survie</h2>
+                <p><?php echo htmlspecialchars($survieBestScore); ?></p>
             </div>
-        </div>
-        <div class="scores-container">
-            <h2>Scores</h2>
-            <?php 
-            $current_theme = null;
-            while ($score = $resultat_scores->fetch_assoc()):
-                if ($score['theme_name'] == 'Survie') {
-                    continue;
-                }
-                if ($score['theme_name'] != $current_theme) {
-                    if ($current_theme !== null) {
-                        echo '</tbody></table>';
-                    }
-                    echo '<h3>' . htmlspecialchars($score['theme_name']) . '</h3>';
-                    echo '<table>
-                            <thead>
-                                <tr>
-                                    <th>Difficulté</th>
-                                    <th>Score</th>
-                                </tr>
-                            </thead>
-                            <tbody>';
-                    $current_theme = $score['theme_name'];
-                }
-            ?>
-                <tr>
-                    <td><?php echo htmlspecialchars($score['difficulty']); ?></td>
-                    <td><?php echo htmlspecialchars($score['score']); ?></td>
-                </tr>
-            <?php endwhile; ?>
-                </tbody>
-            </table>
-            <h2>Meilleur score en mode Survie</h2>
-            <p><?php echo htmlspecialchars($survieBestScore); ?></p>
-        </div>
 
-        <div id="deleteConfirmPopup" class="popup-container" style="display:none;">
-            <div class="popup-content">
-                <h2>Êtes-vous sûr de vouloir définitivement supprimer votre compte ?</h2>
-                <a id="confirmDelete">Supprimer</a>
-                <a id="cancelDelete">Annuler</a>
+            <div id="deleteConfirmPopup" class="popup-container" style="display:none;">
+                <div class="popup-content">
+                    <h2>Êtes-vous sûr de vouloir définitivement supprimer votre compte ?</h2>
+                    <a id="confirmDelete">Supprimer</a>
+                    <a id="cancelDelete">Annuler</a>
+                </div>
             </div>
-        </div>
 
+        </div>
     </div>
 
     <script>
